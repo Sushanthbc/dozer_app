3251e621dcb7c0121af7cd8e852cc109fc774bbe
kslo88 <lohith.lp@gmail.com>
2018-09-16 19:06:04 +0530

    User info (#10)
    * New User Registration Form : First Draft
    
    * userform-stateful
    
    * Splash Page : Condition to check if logged in user is new and displays registration form if found true

---
+++ lib/src/util/user_info.dart
@@ -0,0 +1,19 @@
+part of dozer;
+
+class UserInfo {
+
+  String name;
+  String email;
+  String phone;
+  String about;
+  String purpose;
+
+
+  UserInfo({
+    this.name,
+    this.email,
+    this.phone,
+    this.about,
+    this.purpose
+  });
+}
